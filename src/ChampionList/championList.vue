fa496c203b8da6d145c33e88fa632782e4f25ad6
Abeyy <abey.joy@nyu.edu>
2018-08-13 23:31:27 -0400

    API Work.. probably need to add another promise

--- src/ChampionList/championList.vue
+++ src/ChampionList/championList.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    json: {{apiJSON}}
+    {{ Aatrox }}
   </div>
 </template>
 
@@ -9,12 +9,30 @@ export default {
   name: 'app',
   data () {
     return {
+      //TODO: Add these to champion list
       apiLink: `http://ddragon.leagueoflegends.com/cdn/8.14.1/data/en_US/champion.json`,
-      apiJSON: ''
+      apiJSON: {api: {}},
+      championsList: [],
     }
   },
   mounted() {
-    this.axios.get(this.apiLink).then(response => (this.apiJSON = response))
+    this.axios.get(this.apiLink).then(response => (this.handleAPICall(response)))
+
+    // this.addChampionsToList()
+  },
+  methods: {
+    handleAPICall(response) {
+      this.apiJSON.api = response
+      // this.apiJSON = this.apiJSON.api && this.apiJSON.api.data
+    },
+    addChampionsToList() {
+    }
+  },
+  computed: {
+    Aatrox() {
+      return this.apiJSON.api && this.apiJSON.api.data &&
+            this.apiJSON.api.data && this.apiJSON.api.data.data.Aatrox
+    }
   }
 }
 </script>
