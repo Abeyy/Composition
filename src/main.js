4147e6975e6949f83b56c3d3d1892f7dbe0b06ca
Abey Joy <abey.joy@nyu.edu>
2018-12-04 20:02:27 -0800

    Install popper, bootstrap, and jQuery

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.use(VueAxios, axios)
 
