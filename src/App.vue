90ab8d95d03700af7656f21c9caab2857aa6a10f
Abey Joy <abey.joy@nyu.edu>
2018-12-04 19:58:05 -0800

    Initialize simpleTeamBuilder

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <ChampionList></ChampionList>
+    <!-- <ChampionList></ChampionList> -->
   </div>
 </template>
 
---
+++ src/SimpleTeamBuilder/simpleTeamBuilder.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [],
+    data() { return {} },
+    components: {},
+    mounted() {},
+    computed: {},
+    methods: {},
+    watch: {}
+  }
+</script>
+
+<style scoped>
+</style>
